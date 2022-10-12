8b563bdaddfca80c5fe301ba4f093dae4ec53956
POOJA <https://github.com/Pooja-bhaskaran/Day03.git>
2022-10-12 11:21:56 +0530

    EmployeePayrollApp setup

---
+++ src/main/java/com/example/employeepayroll/EmployeePayRollApplication.java
@@ -0,0 +1,14 @@
+package com.example.employeepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayRollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayRollApplication.class, args);
+        System.out.println("Welcome to employeePayrollApp");
+    }
+
+}
